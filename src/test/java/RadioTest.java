32f381176926151f4c721870522593e56af5ee25
serega <sachernyshov90@mail.ru>
2023-07-15 04:33:58 +0300

    first commit

---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void next() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(6);
+        radio.next();
+        int actual = radio.getCurrentStation();
+        int expected = 7;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prev() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prev();
+        int actual = radio.getCurrentStation();
+        int expected = 9;
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
+
+
